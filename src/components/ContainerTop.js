df40d4d1f38489f291e566348d6d8f70b0368608
Nemanja Vidovic <hefest93@yahoo.com>
2022-02-10 14:55:25 +0100

    added a search warning for limited terms

--- src/components/ContainerTop.js
+++ src/components/ContainerTop.js
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { BsSearch, BsGithub } from "react-icons/bs";
+import { FaExternalLinkAlt } from "react-icons/fa";
 
 const ContainerTop = () => {
+    const [showSearchWarning, setShowSearchWarning] = useState(false);
+
     return (
         <div className="top">
-            <div className="top-logo">RECIPE SEARCH</div>
-            <div className="top-search"></div>
-            <div className="top-links"></div>
+            <div className="top-logo">
+                <p>RECIPE SEARCH</p>
+            </div>
+            <div className="top-search centered">
+                <form className="centered">
+                    <div className="top-search__svg centered">
+                        <BsSearch />
+                    </div>
+                    <input type="text" onFocus={() => setShowSearchWarning(true)} onBlur={() => setShowSearchWarning(false)} />
+                    <button>SEARCH</button>
+
+                    {showSearchWarning && (
+                        <div className="top-search__warning centered-column">
+                            <p>Search terms are limited. Search for terms like "Pizza" or "Chicken".</p>
+                            <a href="https://forkify-api.herokuapp.com/phrases.html" target="_blank" rel="noreferrer" className="centered">
+                                To see the full list of terms, click here. <FaExternalLinkAlt />
+                            </a>
+                        </div>
+                    )}
+                </form>
+            </div>
+            <div className="top-links">
+                <a href="https://www.youtube.com/" target="_blank" rel="noreferrer">
+                    <BsGithub />
+                </a>
+            </div>
         </div>
     );
 };
