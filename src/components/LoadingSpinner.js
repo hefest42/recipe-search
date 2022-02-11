54775aef632281a2890b8ca36df0eba3d2730deb
Nemanja Vidovic <hefest93@yahoo.com>
2022-02-11 12:41:47 +0100

    added loading spinner component

---
+++ src/components/LoadingSpinner.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LoadingSpinner = () => {
+    return (
+        <div className="spinner centered">
+            <div class="lds-dual-ring"></div>
+        </div>
+    );
+};
+
+export default LoadingSpinner;
