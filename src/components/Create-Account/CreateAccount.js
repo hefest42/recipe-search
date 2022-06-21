23f159afe8673c34ed877fab681a7b94820ac98d
Nemanja Vidovic <hefest93@yahoo.com>
2022-06-21 16:43:58 +0200

    connected to google firebase DB

--- src/components/Create-Account/CreateAccount.js
+++ src/components/Create-Account/CreateAccount.js
@@ -1,13 +1,70 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { Link } from "react-router-dom";
 
 import { IoIosCheckmarkCircle, IoMdCloseCircle } from "react-icons/io";
 import { BsFillInfoCircleFill } from "react-icons/bs";
 
-//TODO fix the svg so its in a seperate div for future loading spinner
-//TODO checks for account name as the user types
+const inputStyles = ["form-input", "form-input form-input__correct", "form-input form-input__wrong"];
+
 const CreateAccount = () => {
+    const [allAccounts, setAllAccounts] = useState([]);
+    const [inputedUsername, setInputedUsername] = useState("");
+    const [usernameInputStyle, setUsernameInputStyle] = useState(0);
+    const [passwordInputStyle, setPasswordInputStyle] = useState(0);
+
+    const checkIsUsernameAvailable = () => {
+        const usernameFree = allAccounts.filter(acc => acc.username === inputedUsername);
+
+        console.log(usernameFree);
+    };
+
+    const submitAccountHandler = async e => {
+        e.preventDefault();
+
+        if (usernameInputStyle === 0 || passwordInputStyle === 0) return;
+        if (usernameInputStyle === 2 || passwordInputStyle === 2) return;
+
+        const accountData = {
+            name: "test3",
+            password: "test1234",
+            bookmarks: [""],
+        };
+
+        const response = await fetch(`https://recipedb-3c8b3-default-rtdb.europe-west1.firebasedatabase.app/accounts.json`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(accountData),
+        });
+    };
+
+    // getting all account information
+    useEffect(() => {
+        const getAllAccounts = async () => {
+            try {
+                const response = await fetch(`https://recipedb-3c8b3-default-rtdb.europe-west1.firebasedatabase.app/accounts.json`);
+
+                const data = await response.json();
+
+                const allFetchedAccounts = [];
+
+                for (const key in data) {
+                    allFetchedAccounts.push({
+                        username: data[key].name,
+                        password: data[key].password,
+                        bookmarks: data[key].bookmarks,
+                    });
+                }
+
+                setAllAccounts(allFetchedAccounts);
+            } catch (error) {}
+        };
+
+        getAllAccounts();
+    }, []);
+
     return (
         <div className="create centered">
             <div className="create-inner">
@@ -15,24 +72,36 @@ const CreateAccount = () => {
                 <form className="form">
                     <div className="form-input_container">
                         <label htmlFor="username">Username</label>
-                        <div className="form-input form-input__correct">
-                            <input type="text" id="username" name="username" />
-                            <IoIosCheckmarkCircle />
+                        <div className={inputStyles[usernameInputStyle]}>
+                            <input
+                                type="text"
+                                id="username"
+                                name="username"
+                                onChange={e => {
+                                    setInputedUsername(e.target.value);
+                                    checkIsUsernameAvailable();
+                                }}
+                            />
+                            <div className="centered">
+                                <IoIosCheckmarkCircle />
+                            </div>
                         </div>
                     </div>
 
                     <div className="form-input_container">
                         <label htmlFor="pword">Password</label>
-                        <div className="form-input form-input__wrong">
+                        <div className={inputStyles[passwordInputStyle]}>
                             <input type="password" id="pword" name="pword" autoComplete="yes" />
-                            <IoMdCloseCircle />
+                            <div className="centered">
+                                <IoMdCloseCircle />
+                            </div>
                         </div>
-                        <div className="form-info ">
+                        <div className="form-info">
                             <BsFillInfoCircleFill />
                             <p> needs to be at least 6 characters</p>
                         </div>
                     </div>
-                    <button>Create Account</button>
+                    <button onClick={submitAccountHandler}>Create Account</button>
                 </form>
 
                 <div className="create-inner_info">
