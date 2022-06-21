9bb2dc0558b61321315e6e24940c36712105345a
Nemanja Vidovic <hefest93@yahoo.com>
2022-06-21 17:17:43 +0200

    account creation finished

--- src/components/Create-Account/CreateAccount.js
+++ src/components/Create-Account/CreateAccount.js
@@ -6,41 +6,56 @@ import { IoIosCheckmarkCircle, IoMdCloseCircle } from "react-icons/io";
 import { BsFillInfoCircleFill } from "react-icons/bs";
 
 const inputStyles = ["form-input", "form-input form-input__correct", "form-input form-input__wrong"];
+const svgArr = ["", <IoIosCheckmarkCircle />, <IoMdCloseCircle />];
 
 const CreateAccount = () => {
     const [allAccounts, setAllAccounts] = useState([]);
     const [inputedUsername, setInputedUsername] = useState("");
+    const [inputedPassword, setInputedPassword] = useState("");
     const [usernameInputStyle, setUsernameInputStyle] = useState(0);
     const [passwordInputStyle, setPasswordInputStyle] = useState(0);
-
-    const checkIsUsernameAvailable = () => {
-        const usernameFree = allAccounts.filter(acc => acc.username === inputedUsername);
-
-        console.log(usernameFree);
-    };
 
     const submitAccountHandler = async e => {
         e.preventDefault();
 
-        if (usernameInputStyle === 0 || passwordInputStyle === 0) return;
-        if (usernameInputStyle === 2 || passwordInputStyle === 2) return;
+        if (usernameInputStyle !== 1 || passwordInputStyle !== 1) return;
 
         const accountData = {
-            name: "test3",
-            password: "test1234",
+            name: inputedUsername,
+            password: inputedPassword,
             bookmarks: [""],
         };
 
-        const response = await fetch(`https://recipedb-3c8b3-default-rtdb.europe-west1.firebasedatabase.app/accounts.json`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(accountData),
-        });
+        try {
+            const response = await fetch(`https://recipedb-3c8b3-default-rtdb.europe-west1.firebasedatabase.app/accounts.json`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(accountData),
+            });
+        } catch (error) {
+            console.log(error);
+        }
     };
 
-    // getting all account information
+    // checking for username availability
+    useEffect(() => {
+        const usernameFree = allAccounts.filter(acc => acc.username === inputedUsername);
+
+        if (inputedUsername === "") setUsernameInputStyle(0);
+        if (inputedUsername !== "" && usernameFree.length === 0) setUsernameInputStyle(1);
+        if (inputedUsername !== "" && usernameFree.length > 0) setUsernameInputStyle(2);
+    }, [allAccounts, inputedUsername]);
+
+    // checking for password correctness
+    useEffect(() => {
+        if (inputedPassword.length === 0) setPasswordInputStyle(0);
+        if (inputedPassword.length > 0 && inputedPassword.length < 6) setPasswordInputStyle(2);
+        if (inputedPassword.length >= 6) setPasswordInputStyle(1);
+    }, [inputedPassword]);
+
+    // getting all accounts
     useEffect(() => {
         const getAllAccounts = async () => {
             try {
@@ -59,7 +74,9 @@ const CreateAccount = () => {
                 }
 
                 setAllAccounts(allFetchedAccounts);
-            } catch (error) {}
+            } catch (error) {
+                console.log(error);
+            }
         };
 
         getAllAccounts();
@@ -73,28 +90,16 @@ const CreateAccount = () => {
                     <div className="form-input_container">
                         <label htmlFor="username">Username</label>
                         <div className={inputStyles[usernameInputStyle]}>
-                            <input
-                                type="text"
-                                id="username"
-                                name="username"
-                                onChange={e => {
-                                    setInputedUsername(e.target.value);
-                                    checkIsUsernameAvailable();
-                                }}
-                            />
-                            <div className="centered">
-                                <IoIosCheckmarkCircle />
-                            </div>
+                            <input type="text" id="username" name="username" onChange={e => setInputedUsername(e.target.value)} />
+                            <div className="centered">{svgArr[usernameInputStyle]}</div>
                         </div>
                     </div>
 
                     <div className="form-input_container">
                         <label htmlFor="pword">Password</label>
                         <div className={inputStyles[passwordInputStyle]}>
-                            <input type="password" id="pword" name="pword" autoComplete="yes" />
-                            <div className="centered">
-                                <IoMdCloseCircle />
-                            </div>
+                            <input type="password" id="pword" name="pword" autoComplete="yes" onChange={e => setInputedPassword(e.target.value)} />
+                            <div className="centered">{svgArr[passwordInputStyle]}</div>
                         </div>
                         <div className="form-info">
                             <BsFillInfoCircleFill />
