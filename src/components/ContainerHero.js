c55aa3bd4a5642f6ae2336ff43c3d30a17a175e9
Nemanja Vidovic <hefest93@yahoo.com>
2022-12-26 16:10:03 +0100

    removed random unused components

--- src/components/ContainerHero.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ContainerHero = () => {
-    return <div></div>;
-};
-
-export default ContainerHero;
--- src/components/ContainerList.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ContainerList = () => {
-    return <div></div>;
-};
-
-export default ContainerList;
