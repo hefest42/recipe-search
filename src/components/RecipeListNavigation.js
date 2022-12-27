bd2e14687280ea59e71287a01e4645c5b97c8660
Nemanja Vidovic <hefest93@yahoo.com>
2022-12-27 15:18:33 +0100

    work on pagination

--- src/components/RecipeListNavigation.js
+++ src/components/RecipeListNavigation.js
@@ -6,32 +6,40 @@ import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
 
 const RecipeListNavigation = ({ recipes }) => {
     const params = useParams();
-    const [page, setPage] = useState(0);
+    const [page, setPage] = useState(1);
     const numberOfPages = Math.ceil(recipes.length / 7);
 
-    useEffect(() => {});
+    useEffect(() => {
+        if (params["*"] === "" || recipes.length === 0) return;
+
+        const recipeIndex = recipes.findIndex((recipe) => recipe.recipe_id == params["*"]);
+
+        const pageNumber = recipeIndex === 0 ? 1 : Math.ceil(recipeIndex / 7);
+
+        setPage(pageNumber);
+    }, [params["*"], recipes]);
 
     return (
         <div className="navigation ">
             {(page === 0 && (
                 <>
                     <div className="navigation-empty"></div>
-                    <div>{`Pages: ${page + 1}/${numberOfPages}`} </div>
+                    <div>{`Pages: ${page}/${numberOfPages}`} </div>
                     <AiOutlineArrowRight />
                 </>
             )) ||
                 (page === numberOfPages - 1 && (
                     <>
                         <AiOutlineArrowLeft />
-                        <div>{`Pages: ${page + 1}/${numberOfPages}`} </div>
+                        <div>{`Pages: ${page}/${numberOfPages}`} </div>
                         <div className="navigation-empty"></div>
                     </>
                 )) ||
                 (page > 0 && (
                     <>
                         <AiOutlineArrowLeft />
-                        <div>{`Pages: ${page + 1}/${numberOfPages}`} </div>
-                        <AiOutlineArrowRight o />
+                        <div>{`Pages: ${page}/${numberOfPages}`} </div>
+                        <AiOutlineArrowRight />
                     </>
                 ))}
         </div>
